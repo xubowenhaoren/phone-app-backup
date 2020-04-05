521001f65a7f5ff1b2f7ce7fa7cf86537877754f
K. Shankari <shankari@eecs.berkeley.edu>
2020-04-05 00:06:15 -0700

    Get it to the point where there is no WSOD
    Still not sure that this actually works. Will have to see how the emulator
    deals with this or try it on a real phone.

--- www/js/bluetooth/list.js
+++ www/js/bluetooth/list.js
@@ -1,36 +1,56 @@
 'use strict';
 
 angular
-  .module( 'emission.main.bluetooth.list', [ ] )
-  .controller( 'BluetoothListCtrl', function( $scope, $ionicPlatform, $ionicScrollDelegate ) {
+  .module( 'emission.main.bluetooth.list', [ 'emission.plugin.logger' ] )
+  .controller( 'BluetoothListCtrl', function( $scope, $window, $ionicPlatform,
+        $ionicScrollDelegate, Logger ) {
 
     console.log( 'Controller BluetoothListCtrl called' )
 
-    angular.extend( $scope, { defaults: { zoomControl: false, dragging: false, zoomAnimation: true, touchZoom: false, scrollWheelZoom: false, doubleClickZoom: false, boxZoom: false } } )
+    angular.extend( $scope,
+        { defaults: { zoomControl: false, dragging: false,
+            zoomAnimation: true, touchZoom: false, scrollWheelZoom: false,
+            doubleClickZoom: false, boxZoom: false } } );
 
-    var getContactEvents = _ => {
-      $scope.data = []
-      if ( contacttracing ) {
-        contacttracing.startScanner( null, 10, ce => logContactEvent( ce, false ), e => console.error( 'SCAN', e ) )
-        contacttracing.startAdvertiser( null, ce => logContactEvent( ce, true ), e => console.error( 'AD', e ) )
-        $scope.update = setInterval( contacttracing.updateCEN, 60000 )
-        $scope.refresh = _ => {
-          if ($ionicScrollDelegate.getScrollPosition().top < 20) {
-            clearInterval( $scope.update )
-            contacttracing.stopScanner( scanStopped = e => {
-              e && console.error( e )
-              contacttracing.stopAdvertiser( adStopped = e => {
-                e && console.error( e )
-                getContactEvents()
-              }, adStopped )
-            }, adStopped )
-          }
-        }
+    var startScanningContactEvents = _ => {
+        $scope.data = [];
+        var startScanPromise =
+            $window.contacttracing.startScanner( null, 10)
+            .then(ce => logContactEvent( ce, false ))
+            .catch(e => console.error( 'SCAN', e ));
+        var startAdvPromise =
+          $window.contacttracing.startAdvertiser( null )
+            .then( ce => logContactEvent( ce, true ))
+            .catch( e => console.error( 'AD', e ));
+
+        return Promise.all([startScanPromise, startAdvPromise]) .then(_ => {
+          Logger.log("successfully started scanning, launching CEN update");
+          $scope.update = $window.setInterval( $window.contacttracing.updateCEN,
+            60000 );
+        });
+    }
+
+    var stopScanningContactEvents = _ => {
+        var stopScanPromise = $window.contacttracing.stopScanner();
+        var stopAdvPromise = $window.contacttracing.stopAdvertiser();
+        return Promise.all([stopScanPromise, stopAdvPromise]).then(_ => {
+            Logger.log("successfully stopped scanning");
+            console.log("About to clear interval" + $scope.update);
+        }).catch(err => {
+            Logger.displayError("stopping scan errored", err);
+            console.log("About to clear interval" + $scope.update);
+        });
+    }
+
+    $scope.refresh = _ => {
+      if ($ionicScrollDelegate.getScrollPosition().top < 20) {
+        stopScanningContactEvents().then(startScanningContactEvents());
       }
     }
 
-    var logContactEvent = ( ce, ad ) => $scope.data.push( { time: new Date().toLocaleTimeString(), number: ce.cen.toUpperCase(), ad } )
+    var logContactEvent = ( ce, ad ) => $scope.data.push(
+        { time: new Date().toLocaleTimeString(), number: ce.cen.toUpperCase(), ad }
+    );
 
-    $ionicPlatform.ready().then( getContactEvents )
-
-})
+    // $ionicPlatform.ready().then( startScanningContactEvents() );
+});
